25f8f20e21e7badede53b7087225b243b193f789
FullStackJoshua <Joshualim5131@gmail.com>
2024-12-23 13:17:31 -0500

    fix missing prop for Imgae

--- components/ui/MarqueeItem.tsx
+++ components/ui/MarqueeItem.tsx
@@ -14,12 +14,18 @@ const MarqueeItem: React.FC<MarqueeItemProps> = ({ images, from, to }) => {
       <motion.div
         initial={{ x: `${from}` }}
         animate={{ x: `${to}` }}
-        transition={{ duration: 15, repeat: Infinity, ease: "linear" }}
+        transition={{ duration: 20, repeat: Infinity, ease: "linear" }}
         className="flex flex-shrink-0"
       >
         {images.map((item, index) => (
           <div key={index} className="flex items-center space-x-2 pr-10 h-20 md:h-40 w-fit">
-            <Image className="h-10 w-10 md:h-20 md:w-20" src={item.src} alt={`Icon ${index + 1}`} />
+            <Image
+              src={item.src}
+              alt={`Icon ${index + 1}`}
+              width={40}
+              height={40}
+              className="md:w-20 md:h-20"
+            />
             <span className="text-sm md:text-lg text-white">{item.text}</span>
           </div>
         ))}
@@ -28,13 +34,18 @@ const MarqueeItem: React.FC<MarqueeItemProps> = ({ images, from, to }) => {
       <motion.div
         initial={{ x: `${from}` }}
         animate={{ x: `${to}` }}
-        transition={{ duration: 15, repeat: Infinity, ease: "linear" }}
+        transition={{ duration: 20, repeat: Infinity, ease: "linear" }}
         className="flex flex-shrink-0"
       >
         {images.map((item, index) => (
           <div key={index} className="flex items-center space-x-2 pr-10 h-20 md:h-40 w-fit">
-            <Image className="h-10 w-10 md:h-20 md:w-20" src={item.src} alt={`Icon ${index + 1}`} />
-
+            <Image
+              src={item.src}
+              alt={`Icon ${index + 1}`}
+              width={40}
+              height={40}
+              className="md:w-20 md:h-20"
+            />
             <span className="text-sm md:text-lg text-white">{item.text}</span>
           </div>
         ))}
